e7a246db74a36c462d72134447c1080edb288c22
YuriMendess <contato.yurimendess@gmail.com>
2022-02-23 22:56:35 -0300

    add variables styles

--- src/Styles/Variables.ts
+++ src/Styles/Variables.ts
@@ -1,1 +1,54 @@
-export const header_height = `3rem`;
+export const header_height: string = `3rem`;
+
+/*========== Colors ==========*/
+export const color: number = 206;
+export const back_color: string = `hsl(${color}, 4%, 4%)`;
+export const back_color_alt: string = `hsl(${color}, 4%, 8%)`;
+export const title_color: string = `hsl(${color}, 4%, 95%),`;
+export const text_color: string = `hsl(${color}, 4%, 75%)`;
+export const text_color_light: string = `hsl(${color}, 4%, 65%)`;
+export const white_color: string = `#FFF`;
+export const body_color: string = `hsl(${color}, 4%, 6%)`;
+export const container_color: string = `hsl(${color}, 4%, 10%)`;
+export const text_gradient: string = `linear-gradient(hsl(${color}, 4%, 24%), hsl(${color}, 4%, 8%))`;
+export const scroll_thumb_color: string = `hsl(${color}, 4%, 16%)`;
+export const scroll_thumb_color_alt: string = `hsl(${color}, 4%, 20%)`;
+
+/*========== Font and typography ==========*/
+export const body_font: string = `Poppins, sans-serif`;
+export const biggest_font_size: string = `5rem`;
+export const bigger_font_size: string = `3.5rem`;
+export const big_font_size: string = `2.5rem`;
+export const h2_font_size: string = `1.25rem`;
+export const h3_font_size: string = `1.125rem`;
+export const normal_font_size: string = `.938rem`;
+export const small_font_size: string = `.813rem`;
+export const smaller_font_size: string = `.75`;
+export const text_line_height: string = `2rem`;
+
+/*
+export const biggest_font_size: string = `7.5rem`;
+export const bigger_font_size: string = `4.5rem`;
+export const big_font_size: string = `4rem`;
+export const h2_font_size: string = `1.5rem`;
+export const h3_font_size: string = `1.25rem`;
+export const normal_font_size: string = `1rem`; 
+export const small_font_size: string = `875rem`;
+export const smaller_font_size: string = `.813rem`;
+*/
+
+/*========== Font Weight ==========*/
+export const font_medium: number = 500;
+export const font_semi_bold: number = 600;
+
+/*========== Margins Bottom ==========*/
+export const mb_0_5: string = `.5rem`;
+export const mb_0_75: string = `.75rem`;
+export const mb_1: string = `1rem`;
+export const mb_1_5: string = `1.5rem`;
+export const mb_2: string = `2rem`;
+export const mb_2_5: string = `2.5rem`;
+
+/*========== Z index ==========*/
+export const z_tooltip: number = 10;
+export const z_fixed: number = 100;
