00319e02906ec8b4f15a249d61d48c981e5c86d4
YuriMendess <contato.yurimendess@gmail.com>
2022-02-23 22:25:07 -0300

    removing and add files

---
+++ src/Components/Header/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../Assets/img/logo.png';
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<img src={logo}/>
+			
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/Styles/Variables.ts
@@ -0,0 +1,1 @@
+export const header_height = `3rem`;
