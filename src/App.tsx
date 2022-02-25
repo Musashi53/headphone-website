4eebbf9d774ae70f7b3234d1c52b7f94b90a2157
YuriMendess <contato.yurimendess@gmail.com>
2022-02-25 16:07:12 -0300

    add components in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { GlobalStyle, Main } from './Styles/GlobalStyle';
 import Header from './Components/Header/Index';
 import Home from './Components/Home';
-
+import Sponsor from './Components/Sponsor/index';
+import Specs from './Components/Specs/index';
+import Cases from './Components/Case';
+import Discount from './Components/Discount';
+import Products from './Components/Products';
+import Footer from './Components/Footer';
 
 function App(): JSX.Element {
   return (
@@ -11,6 +16,12 @@ function App(): JSX.Element {
       <Header/>
       <Main>
         <Home/>
+        <Sponsor/>
+        <Specs/>
+        <Cases/>
+        <Discount/>
+        <Products/>
+        <Footer/>
       </Main>
     </div>
   );
